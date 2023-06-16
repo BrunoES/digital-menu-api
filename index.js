a8d6a8b0d8f92e3f12f24556914f54b1b936be35
BrunoES <brunoh915@gmail.com>
2023-06-15 22:53:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const pathImages = `.${sep}${sep}media${sep}${sep}imgs`;
 const pathQRCodes = `.${sep}${sep}media${sep}${sep}qrcodes`;
 
 const BASE_IP = 'localhost';
-const BASE_IP_DB = 'mysql://ws1m9b9fv4hcra1sb3mx:pscale_pw_n5S0N74XgeJky6BN50EnwwzV2DTNugUYiiYTMHPe6xz@aws.connect.psdb.cloud/digital_menu?ssl={"rejectUnauthorized":true}';
+const BASE_IP_DB = 'mysql://mcc91ioe14v4p229tdz6:pscale_pw_s8DATfEx8kEToEFERHlsYx0wtV0IFjOMXtFCq0KU0PV@aws.connect.psdb.cloud/digital_menu?ssl={"rejectUnauthorized":true}';
 
 const BASE_URL_QRCODE_MESA = `http://${BASE_IP}:3000`;
 //const BASE_URL_QRCODE_MESA = 'http://192.168.0.18:3000';
